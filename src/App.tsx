5d578ce71ae15a48504f82349f645efbe86b03a4
ichiragkumar <zkichirag@gmail.com>
2025-11-02 02:43:01 +0530

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Homepage } from './components/Homepage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>  
